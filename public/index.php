b375d1f93aeb565cd9c644359e31214952746295
DanielRojoR <danielrojor1999@gmail.com>
2025-05-12 02:33:48 -0400

    Project-structure-setup

---
+++ public/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/assets/css/base/main.css">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <div class="nav-wrapper">
+            <div class="nav-container">
+                <div class="nav-left">
+
+                </div>
+                <div class="nav-right">
+
+                </div>
+            </div>
+        </div>
+    </nav>
+    <main>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque accusantium quas unde perspiciatis optio sequi? Alias quae explicabo repudiandae velit! Dignissimos perferendis perspiciatis minima, assumenda quaerat voluptatem neque laudantium magnam, esse, libero ut sint temporibus a porro numquam quam. Quo.</p>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
