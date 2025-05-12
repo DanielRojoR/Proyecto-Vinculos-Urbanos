6388e134c00ee010207fb58c69b355bfec585f85
DanielRojoR <danielrojor1999@gmail.com>
2025-05-12 04:36:09 -0400

    website v0.01
    empezando con la estructura de los estilos

--- public/index.php
+++ public/index.php
@@ -3,27 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/assets/css/base/main.css">
+    <link rel="stylesheet" href="\public\assets\css\main.css">
     <title>Document</title>
 </head>
 <body>
-    <nav>
-        <div class="nav-wrapper">
-            <div class="nav-container">
-                <div class="nav-left">
-
-                </div>
-                <div class="nav-right">
-
-                </div>
-            </div>
+    <nav class="navbar">
+        <div class="container navbar-container">
+        <div class="navbar-logo">
+            <a href="/">Company</a>
+        </div>
+        
+        <button class="navbar-toggle" aria-label="Toggle navigation">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <line x1="3" y1="12" x2="21" y2="12"></line>
+                <line x1="3" y1="6" x2="21" y2="6"></line>
+                <line x1="3" y1="18" x2="21" y2="18"></line>
+            </svg>
+        </button>
+        
+        <ul class="navbar-links">
+            <li><a href="/" class="active">Home</a></li>
+            <li><a href="/features">Features</a></li>
+            <li><a href="/pricing">Pricing</a></li>
+            <li><a href="/about">About</a></li>
+            <li><button class="navbar-button">Get Started</button></li>
+        </ul>
         </div>
     </nav>
     <main>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque accusantium quas unde perspiciatis optio sequi? Alias quae explicabo repudiandae velit! Dignissimos perferendis perspiciatis minima, assumenda quaerat voluptatem neque laudantium magnam, esse, libero ut sint temporibus a porro numquam quam. Quo.</p>
     </main>
     <footer>
-
     </footer>
 </body>
 </html>
